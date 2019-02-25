9735269d43a8548ce8e3e96f20c9ec59a3fa83d7
GYDeveloper <developergyd@gmail.com>
2019-02-25 14:49:45 +0700

    initial commit

---
+++ src/Loading.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+// const Loading = props => <h2>{props.message}</h2>;
+const Loading = ({ message }) => <h2>{message}</h2>;
+
+// export more than 1 component
+// export const Loading = () => <h2>Loading</h2>;
+
+// export only 1 component
+export default Loading;
